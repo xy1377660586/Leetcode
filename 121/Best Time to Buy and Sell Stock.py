4fa2f81a26bd3bf26ab15fe414a3daa47096b328
xy1377660586 <bupt_xy@126.com>
2017-01-09 13:01:52 -0400

    Update Best Time to Buy and Sell Stock.py

--- 121/Best Time to Buy and Sell Stock.py
+++ 121/Best Time to Buy and Sell Stock.py
@@ -5,16 +5,13 @@ class Solution(object):
 		:type prices: List[int]
 		:rtype: int
 		"""
-		if prices==[]:
-			return 0
+		
 		day=len(prices)
-		min_value=min(prices)
-		N=prices.index(min_value)
-		if N==day-1:
+		if day<2:
 			return 0
-		for i in range(N+1,day):
-			max_value=prices[i-1]
-			if prices[i]>max_value:
-				max_value=prices[i]
-			profit=max_value-min_value
-		return profit
+		maxprofit=0
+		curMin=prices[0]
+		for i in range(1,day):
+			CurMin=min(CurMin,prices[i])
+			maxprofit=max(maxprofit,prices[i]-CurMin)
+		return maxprofit
