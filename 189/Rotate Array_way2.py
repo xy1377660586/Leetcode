2960ba8c9fc4aa1723dc0e4fcfc52c54346b90a3
xy1377660586 <bupt_xy@126.com>
2017-01-09 18:26:42 -0400

    Update Rotate Array_way2.py

--- 189/Rotate Array_way2.py
+++ 189/Rotate Array_way2.py
@@ -9,4 +9,10 @@ class Solution(object):
 		if n==0:
 			return nums
 		
-	#在LeetCode提交的时候报错！！！在python里面跑的时候么有！！！
+	#在LeetCode提交的时候报错！！！在python里面跑的时候么有！！！原因在于不要写return nums，可是why？？
+class Solution(object):
+    def rotate(self, nums, k):
+        n = len(nums)
+        if k > 0 and n > 1:
+            nums[:] = nums[n - k:] + nums[:n - k]
+       
