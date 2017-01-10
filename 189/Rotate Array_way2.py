66edc093809edbb91634dd57027e432c7d53412e
xy1377660586 <bupt_xy@126.com>
2017-01-10 14:35:45 -0400

    Update Rotate Array_way2.py

--- 189/Rotate Array_way2.py
+++ 189/Rotate Array_way2.py
@@ -7,12 +7,7 @@ class Solution(object):
 			nums=nums[n-k :]+nums[: k]
 		return nums
 		if n==0:
-			return nums
-		
-	#在LeetCode提交的时候报错！！！在python里面跑的时候么有！！！原因在于不要写return nums，可是why？？
-class Solution(object):
-    def rotate(self, nums, k):
-        n = len(nums)
-        if k > 0 and n > 1:
-            nums[:] = nums[n - k:] + nums[:n - k]
+			#return nums 这个语句不要啊因为不要求return anything
+			
+#
        
