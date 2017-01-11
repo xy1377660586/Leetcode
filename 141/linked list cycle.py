c0534c416ab14b18c41ee16979fcb2dc641d1310
xy1377660586 <bupt_xy@126.com>
2017-01-11 12:25:50 -0400

    Update linked list cycle.py

--- 141/linked list cycle.py
+++ 141/linked list cycle.py
@@ -5,3 +5,26 @@ fast超了slow一圈，返回true
 面，fast在后面每次循环都向slow靠近1，所以一定会相遇，而不会出现fast直接跳过slow的情况
 
 快慢指针
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+	def hasCycle(self, head):
+		
+		if head==None or head.next==None:
+			return False
+			
+		q,s=head,head
+		while q and q.next: # 注意这个条件，如果只是用while q.next 是不可以的
+		#这是英文这两指针， P 和s 中的p 是每次走两步的，所以的话，q！=None和q.next！=None必须同时满足
+			q=q.next.next
+			
+			s=s.next
+
+			if q==s:
+				return True
+		return False
