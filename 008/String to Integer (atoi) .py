b826e544d9f2fb81a924803f695a7cc7d41699f1
xy1377660586 <bupt_xy@126.com>
2017-01-16 10:22:10 -0500

    Update String to Integer (atoi) .py

--- 008/String to Integer (atoi) .py
+++ 008/String to Integer (atoi) .py
@@ -8,7 +8,7 @@ class Solution(object):
         sign = 1
         ret = 0
         length = len(str)
-        MaxInt = (1 << 31) - 1
+        MaxInt = (1 << 31) - 1 #这里俄解释在下面俄链接里面可以看到:注意<<的优先级低于-
         if str[i] == '+':
             i += 1
         elif str[i] == '-' :
@@ -16,10 +16,10 @@ class Solution(object):
             sign = -1
         
         for i in range(i, length) :
-            if str[i] < '0' or str[i] > '9' : #这个条件的作用呢? 为啥?
+            if str[i] < '0' or str[i] > '9' : #这个条件的作用呢? 为啥?是不是因为ascall表中的位置?
                 break
             ret = ret * 10 + int(str[i])
-            if ret > sys.maxint:#sys.maxint 是什么?
+            if ret > sys.maxint:#sys.maxint 是什么?需要导入import sys ,而? sys.maxint 的值是 9223372036854775807? 为什么是大于呢?
                 break
         ret *= sign
         if ret >= MaxInt:
@@ -27,3 +27,7 @@ class Solution(object):
         if ret < MaxInt * -1 :
             return MaxInt * - 1 - 1 #补码表示的问题
         return ret
+    
+    
+    #https://zhidao.baidu.com/question/176374554.html 关于取值范围的解释:
+    #http://wenku.baidu.com/view/208bb2f20b4e767f5bcfceae.html?re=view
