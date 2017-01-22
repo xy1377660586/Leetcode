440f32fb1cbc892a1bf30899f498f50b588522fc
xy1377660586 <bupt_xy@126.com>
2017-01-22 18:21:56 -0400

    Update Contains Duplicate II.py

--- 219/Contains Duplicate II.py
+++ 219/Contains Duplicate II.py
@@ -1,3 +1,5 @@
+遍历所有元素，将元素值当做键、元素下标当做值，存放在一个字典中。遍历的时候，
+如果发现重复元素，则比较其下标的差值是否小于k，如果小于则可直接返回True，否则更新字典中该键的值为新的下标。
 class Solution(object):
     def containsNearbyDuplicate(self, nums, k):
         """
@@ -5,16 +7,10 @@ class Solution(object):
         :type k: int
         :rtype: bool
         """
-        windows=[]
-        if len(nums)<k:
-           return False 
- 
-        while len(nums)==k:
-            if nums[0]==nums[-1]: 报错奇葩
+        num_map={}
+        for i in range(len(nums)):
+            if nums[i] in num_map and i-num_map[nums[i]]<=k:
                 return True
-            
-        for i in range(len(nums)-k):
-            windows=nums[i+1:i+k+1] 
-            if nums[i] in windows:
-                return True
+            else:
+                num_map[nums[i]]=i
         return False
