5c3aef9c4e549a4bcdf26639bb2f0b43c7e08e2d
xy1377660586 <bupt_xy@126.com>
2017-01-22 23:01:28 -0400

    Update Implement strStr() _2.py

--- 028/Implement strStr() _2.py
+++ 028/Implement strStr() _2.py
@@ -1,21 +1,16 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        """
-        :type haystack: str
-        :type needle: str
-        :rtype: int
-        """
-        if len(needle)>len(haystack):
+class Solution:
+    def strStr(self, source, target):
+        if source is None or target is None:
             return -1
-        if len(needle)<=len(haystack):
-            for i in range(len(haystack)):
-                if haystack[i]==needle[0]:
-                    index=i
-                if len(haystack)>=i+len(needle) and haystack[i:i+len(needle)]==needle[0:len(needle)]:
-                    return index
-            return -1
-a=Solution()
-ss='weffsddvds'
-ee='ffa'
-print a.strStr(ss,ee)
-#自己的写法，为什么在leetcode上报错
+        lens=len(source)
+        lent=len(target)
+        for i in range (lens-lent+1):
+            j=0
+            while(j<lent):
+                if source[i+j]!=target[j]:
+                    break;
+                j=j+1;
+            if j==lent:
+                return i
+        return -1
+
