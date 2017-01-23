4fa5b5a5bdef16bde42605d2a67f146ce0b5a012
xy1377660586 <bupt_xy@126.com>
2017-01-22 22:06:26 -0400

    Update Intersection of Two Linked Lists.py

--- 160/Intersection of Two Linked Lists.py
+++ 160/Intersection of Two Linked Lists.py
@@ -1,59 +1,42 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, x):
 #         self.val = x
 #         self.next = None
 
-class Solution(object):
-	def getIntersectionNode(self, headA, headB):
-		while headA:
-				headA=headA.next
-				headB=headB.next
-		for i in range(3):
-			headB=headB.next#为什么会有这种奇葩的报错！！！！
-###################################
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class Solution:
+    # @param headA: the first list
+    # @param headB: the second list
+    # @return: a ListNode
+    def getIntersectionNode(self, headA, headB):
+        # Write your code here
+        listA = []
+        listB = []
+        if headA == None or headB == None:
+            return None
+        while 1:
+            if headA == None:
+                break;
+            listA.append(headA.val)
+            headA = headA.next
 
-class Solution(object):
-	def getIntersectionNode(self, headA, headB):
-		if headA==None or headB==None:
-			return None
+        while 1:
+            if headB == None:
+                break;
+            listB.append(headB.val)
+            headB = headB.next
 
-		listA=[]
-		listB=[]
-		
-		while 1:
-			if headA==None:
-				break;
-			listA.append(headA.val)
-			headA=headA.next
+        if listA[-1] != listB[-1]:
+            return None;
+        
+        if len(listA)<len(listB):
+            minLen = len(listA)
+        else:
+            minLen = len(listB)
 
-		while 1:
-			if headB==None:
-				break;
-			listB.append(headB.val)
-			headB=headB.next
-
-
-		if listA[-1]!=listB[-1]:
-			return None;
-
-		if len(listA)<len(listB):
-			diff=len(listB)-len(listA)
-			
-			for i in range(diff):
-				headB=headB.next	
-			while headA:
-				headA=headA.next
-				headB=headB.next
-				if headA.val==headB.val:
-					return headA.val
-				else:
-					return None;
-
-
-	
+        inster = []
+        for i in range(1,minLen+1):
+            if listA[-i] != listB[-i]:
+                return ListNode(listA[-i+1])
+            if i == minLen:
+                return ListNode(listA[-i])
