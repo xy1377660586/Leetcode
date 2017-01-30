013dc23f35fd9ec53b2252f2604c01e6523a9eba
xy1377660586 <bupt_xy@126.com>
2017-01-30 18:46:47 -0400

    Create  Merge Sorted Array_2 .py

---
+++ 088/ Merge Sorted Array_2 .py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def merge(self, A, m, B, n): 
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: void Do not return anything, modify nums1 in-place instead.
+        """
+        A[m:]=B[:n]
+        A.sort()
+        
+      
+        
+      
