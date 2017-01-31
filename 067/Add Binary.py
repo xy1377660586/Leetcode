712a2c0b10d1748bac9043ca11c3c0cd64a519ac
Yan <bupt_xy@126.com>
2017-01-31 12:56:06 -0500

    Create Add Binary.py

---
+++ 067/Add Binary.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        return bin(int(a,2)+int(b,2))[2:]
+        %为什么[2:]?
+        因为输出的前两个数是0b
